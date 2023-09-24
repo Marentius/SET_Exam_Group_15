c9e29a346ab5d16b2898e4b6cc017092dd88bbe6
marentius <vetlenilseen@hotmail.com>
2023-09-24 15:43:08 +0200

    Endret størrelse på GUI

--- src/main/java/com/set/guide/FirstSwingExample.java
+++ src/main/java/com/set/guide/FirstSwingExample.java
@@ -5,12 +5,12 @@ public class FirstSwingExample {
     public static void main(String[] args) {
         JFrame f=new JFrame();//creating instance of JFrame
 
-        JButton b=new JButton("Trykk her");//creating instance of JButton
-        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        JButton b=new JButton("Click here");//creating instance of JButton
+        b.setBounds(200,200,200, 80);//x axis, y axis, width, height
 
         f.add(b);//adding button in JFrame
 
-        f.setSize(400,500);//400 width and 500 height
+        f.setSize(800,900);//400 width and 500 height
         f.setLayout(null);//using no layout managers
         f.setVisible(true);//making the frame visible
     }
