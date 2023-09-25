c587a331d82244e6f74280a74e44f570dbd4a953
marentius <vetlenilseen@hotmail.com>
2023-09-25 08:38:26 +0200

    Endret variabelnavn inne i testene, og la til en @BeforeEach initialisering

--- src/test/java/com/set/guide/FunctionsTests.java
+++ src/test/java/com/set/guide/FunctionsTests.java
@@ -1,37 +1,44 @@
 package com.set.guide;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FunctionsTests {
 
-    private TripList tripList = new TripList();
+    final TripList tripList = new TripList();
+    private Trip oslo;
+    private Trip sarp;
+
+    @BeforeEach
+    public void init(){
+        oslo = new Trip("Oslo", 100);
+        sarp = new Trip("Sarpsborg", 200);
+    }
 
     @Test
     public void book_trip_test() {
-        int count1 = tripList.getTripList().size();
+        int originalTriplist = tripList.getTripList().size();
 
-        tripList.addTrip(new Trip("Oslo", 100));
+        tripList.addTrip(oslo);
 
-        int count2 = tripList.getTripList().size();
+        int changedTriplist = tripList.getTripList().size();
 
-        assertTrue(count1 < count2);
+        assertTrue(originalTriplist < changedTriplist);
     }
 
     @Test
     public void cancel_booking_test() {
-        Trip oslo = new Trip("Oslo", 100);
-        Trip sarp = new Trip("Sarp", 200);
         tripList.addTrip(oslo);
         tripList.addTrip(sarp);
 
-        int count1 = tripList.getTripList().size();
+        int originalTriplist = tripList.getTripList().size();
 
         tripList.deleteTrip(oslo);
 
-        int count2 = tripList.getTripList().size();
+        int changedTriplist = tripList.getTripList().size();
 
-        assertTrue(count1 > count2);
+        assertTrue(originalTriplist > changedTriplist);
     }
 }
