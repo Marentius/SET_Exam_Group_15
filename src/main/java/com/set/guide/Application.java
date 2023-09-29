94fcd5acfbcc3d28b6473f1010cc7fe8204f5818
Marentius <vetlenilseen@hotmail.com>
2023-09-29 16:15:30 +0200

    Gjort endringer på GUI. Ikke ferdig ofc, men kommet godt på vei. Ring meg om du lurer på noe Robin

--- src/main/java/com/set/guide/Application.java
+++ src/main/java/com/set/guide/Application.java
@@ -1,8 +1,12 @@
 package com.set.guide;
 
+import com.set.guide.forms.LogIn;
+
+import javax.swing.*;
+
 public class Application {
     public static void main(String[] args) {
-        //LogIn.createLogInWindow();
+        LogIn logIn = new LogIn("Log in");
     }
 }
 
---
+++ src/main/java/com/set/guide/forms/HomePage.java
@@ -0,0 +1,12 @@
+package com.set.guide.forms;
+
+import javax.swing.*;
+
+public class HomePage extends InitGui{
+    private JPanel mainPanel;
+
+    public HomePage(String title) {
+        super(title);
+        start_gui(mainPanel);
+    }
+}
