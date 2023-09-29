bd3b6cb8ff1ff24ed2cf2f6c509834d5e3b5fc51
Marentius <vetlenilseen@hotmail.com>
2023-09-29 11:34:31 +0200

    Egen branch til development. Ikke push kode inn hit før koden er ferdig. Dette vil være mellomstasjonen koden lever i før den endelige koden blir merget til master. Kode skal altså merges inn hit, ikke master når kode er ferdig.

--- src/main/java/com/set/guide/Application.java
+++ src/main/java/com/set/guide/Application.java
@@ -1,9 +1,6 @@
 package com.set.guide;
 
-import com.set.guide.forms.HomePage;
 import com.set.guide.forms.LogIn;
-import com.set.guide.models.Trip;
-import com.set.guide.models.User;
 
 public class Application {
     public static void main(String[] args) {
