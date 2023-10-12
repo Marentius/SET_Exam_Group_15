e0c053b02376cb86a7e84d19a63a3b4582e8db00
Marentius <vetlenilseen@hotmail.com>
2023-10-12 21:18:39 +0200

    test

--- src/main/java/com/set/mvp/Application.java
+++ src/main/java/com/set/mvp/Application.java
@@ -6,6 +6,7 @@ import com.set.mvp.panels.StartPanelLogIn;
 public class Application {
     public static void main(String[] args) {
         StartPanelLogIn startPanelLogIn = new StartPanelLogIn("Log in");
+        //Hei
     }
 }
 
