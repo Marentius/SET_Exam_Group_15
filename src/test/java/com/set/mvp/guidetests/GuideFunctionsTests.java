f564ca78108266a129ef3529e23f221e60ae05c8
Marentius <vetlenilseen@hotmail.com>
2023-10-17 17:50:11 +0200

    Lagt til Github Actions

--- src/test/java/com/set/mvp/guidetests/GuideFunctionsTests.java
+++ src/test/java/com/set/mvp/guidetests/GuideFunctionsTests.java
@@ -1,7 +1,12 @@
 package com.set.mvp.guidetests;
 
+import com.set.mvp.models.Guide;
+import com.set.mvp.models.Trip;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class GuideFunctionsTests {
@@ -12,22 +17,42 @@ public class GuideFunctionsTests {
 
     @Test
     public void guide_can_create_trip(){
-        fail("Method not implemented yet.");
+        fail("ikke implementert");
     }
 
-    @Test
+    /*@Test
     public void guide_can_cancel_trip(){
-        fail("Method not implemented yet.");
-    }
+        ArrayList<Trip> trips = new ArrayList<>();
+        Guide guide = new Guide("Guide", "Guide", "Guide", "Guide", "Guide", 100, trips);
+
+        Trip trip = new Trip("Oslo", 100);
+        Trip trip1 = new Trip("Tønsberg", 100);
+
+        guide.addTrip(trip);
+        guide.addTrip(trip1);
+
+        int tripListSize = guide.getTrips().size();
+
+        guide.deleteTrip(trip);
+
+        assertTrue(tripListSize > guide.getTrips().size());
+    }*/
     @Test
     public void guide_can_contact_support(){
         fail("Method not implemented yet");
     }
 
-    @Test
+    /*@Test
     public void guide_can_edit_trip(){
-        fail("Method not implemented yet");
-    }
+        ArrayList<Trip> trips = new ArrayList<>();
+        Guide guide = new Guide("Guide", "Guide", "Guide", "Guide", "Guide", 10000,trips);
+        Trip trip = new Trip("Oslo", 100);
+        guide.addTrip(trip);
+
+        //guide.editTrip(trip);
+
+        fail("Ikke ferdig");
+    }*/
 
     @Test
     public void guide_can_report_user(){
