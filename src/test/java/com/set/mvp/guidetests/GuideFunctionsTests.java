2cfa4f87127f174d3a261bf092cec630d94d4203
robin <robin.enerhaugen@hotmail.com>
2023-10-19 19:38:11 +0200

    lagd en test for guide_can_create_trip, kommentert ut ettersom det ikke er en implementert metode som lar guide lage en trip, lagd sånn jeg tenker deg kommer til å se ut etter implementasjon

--- src/test/java/com/set/mvp/guidetests/GuideFunctionsTests.java
+++ src/test/java/com/set/mvp/guidetests/GuideFunctionsTests.java
@@ -2,23 +2,55 @@ package com.set.mvp.guidetests;
 
 import com.set.mvp.models.Guide;
 import com.set.mvp.models.Trip;
+
+import com.set.mvp.repository.GuideJsonRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GuideFunctionsTests {
-    /*@Test
-    public void guide_can_create_profile(){
-        fail("Method not implemented yet.");
-    }
+    GuideJsonRepository guideJsonRepository;
+    @BeforeEach
+        public void init(){
+           guideJsonRepository = new GuideJsonRepository("/database/guide.json");
+
+        }
+
 
     @Test
-    public void guide_can_create_trip(){
-        fail("ikke implementert");
+    public void guide_can_create_profile() {
+        ArrayList<Guide> guidesOriginal = guideJsonRepository.getGuides();
+        Guide createdGuide = guideJsonRepository.createGuide("Guide", "Guide", "Guide", "Guide", "Guide", null);
+        boolean isProfileIdInList = false;
+        for (Guide guide : guideJsonRepository.getGuides()) {
+            if (guide.getProfileId() == createdGuide.getProfileId()) {
+                isProfileIdInList = true;
+                break;
+            }
+
+        }
+        assertTrue(isProfileIdInList);
+        assertFalse(Arrays.equals(guidesOriginal.toArray(), guideJsonRepository.getGuides().toArray()));
+
+        guideJsonRepository.deleteGuide(createdGuide.getProfileId());
     }
+
+    /*
+    @Test
+    public void guide_can_create_trip(){
+        ArrayList<Trip> trips = new ArrayList<>();
+    Guide createdGuide = guideJsonRepository.createGuide("Guide","Guide","Guide","Guide","Guide", trips);
+    ArrayList<Trip> originalTripList = createdGuide.getTrips();
+    createdGuide.createTrip("Trip","Trip","Trip","Trip", 200);
+    ArrayList<Trip> newTripList = createdGuide.getTrips();
+    assertFalse(originalTripList != newTripList);
+    }
+
+     */
 
     @Test
     public void guide_can_cancel_trip(){
@@ -37,10 +69,12 @@ public class GuideFunctionsTests {
 
         assertTrue(tripListSize > guide.getTrips().size());
     }
-
+/*
    @Test
     public void guide_can_edit_trip(){
-        fail("Ikke ferdig");
-    }*/
+
+    }
+
+ */
 
 }
