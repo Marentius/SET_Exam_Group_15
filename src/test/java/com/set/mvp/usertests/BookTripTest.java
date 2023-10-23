90765e4169745f0a4ac7160ec3cc8f66130397f4
Enerhaugen <robin.enerhaugen@hotmail.com>
2023-10-23 08:57:38 +0200

    lagd tester for book one trip, book several trips

--- src/test/java/com/set/mvp/usertests/BookTripTest.java
+++ src/test/java/com/set/mvp/usertests/BookTripTest.java
@@ -2,45 +2,61 @@ package com.set.mvp.usertests;
 
 import com.set.mvp.models.Trip;
 import com.set.mvp.models.User;
+import com.set.mvp.repository.TripJsonRepository;
+import com.set.mvp.repository.UserJsonRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookTripTest {
+    UserJsonRepository userJsonRepository;
+    TripJsonRepository tripJsonRepository;
 
-    //final User userList = new User("Default", "Default", "Default", "Default", "Default", 1234);
-    //private Trip oslo;
-
-    /*@BeforeEach
+    @BeforeEach
     public void init(){
-        oslo = new Trip("Oslo", 100);
+    userJsonRepository = new UserJsonRepository("/database/user.json");
+    tripJsonRepository = new TripJsonRepository("/database/trip.json");
     }
 
     @Test
     public void book_one_trip_test() {
-        int originalTriplist = userList.getTrips().size();
+        ArrayList<Trip> trips = new ArrayList<>();
+        User user = userJsonRepository.createUser("User", "User", "User", "User", "User", trips);
+        Trip trip = tripJsonRepository.addTrip("Trip", "Trip", "Trip", null, 100, 100, null, null);
+        user.addTrip(trip);
+        assertTrue(user.getTrips().size() > 0);
+        assertTrue(user.getTrips().size() < 2);
+        assertFalse(user.getTrips().size() == 3);
+        tripJsonRepository.deleteTrip(trip.getTripId());
+        userJsonRepository.deleteUser(user.getProfileId());
 
-        userList.addTrip(oslo);
-
-        int changedTriplist = userList.getTrips().size();
-
-        assertTrue(originalTriplist < changedTriplist);
     }
 
     @Test
     public void book_several_trips_test() {
-        int originalTriplist = userList.getTrips().size();
+        ArrayList<Trip> trips = new ArrayList<>();
+        User user = userJsonRepository.createUser("User", "User", "User", "User", "User", trips);
+        Trip trip1 = tripJsonRepository.addTrip("Trip", "Trip", "Trip", null, 100, 100, null, null);
+        Trip trip2 = tripJsonRepository.addTrip("Trip", "Trip", "Trip", null, 100, 100, null, null);
+        Trip trip3 = tripJsonRepository.addTrip("Trip", "Trip", "Trip", null, 100, 100, null, null);
+        Trip trip4 = tripJsonRepository.addTrip("Trip", "Trip", "Trip", null, 100, 100, null, null);
 
-        userList.addTrip(oslo);
-        userList.addTrip(oslo);
-        userList.addTrip(oslo);
-        userList.addTrip(oslo);
+        user.addTrip(trip1);
+        user.addTrip(trip2);
+        assertTrue(user.getTrips().size() == 2);
+        user.addTrip(trip3);
+        assertFalse(user.getTrips().size() == 2);
+        user.addTrip(trip4);
+        assertTrue(user.getTrips().size() == 4);
 
-        int changedTripList = userList.getTrips().size();
-
-        assertTrue(originalTriplist < changedTripList);
-    }*/
+        userJsonRepository.deleteUser(user.getProfileId());
+        tripJsonRepository.deleteTrip(trip1.getTripId());
+        tripJsonRepository.deleteTrip(trip2.getTripId());
+        tripJsonRepository.deleteTrip(trip3.getTripId());
+        tripJsonRepository.deleteTrip(trip4.getTripId());
+    }
 
 }
